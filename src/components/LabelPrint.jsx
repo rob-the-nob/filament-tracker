8bdd299c273fc39bd69064ed9cd7658460d23bca
rob-the-nob <robinarmitage2010@gmail.com>
2026-06-12 19:42:04 +0100

    t

--- src/components/LabelPrint.jsx
+++ src/components/LabelPrint.jsx
@@ -1,50 +1,59 @@
-export default function LabelPrint({ item }) {
-  const barcode = String(item.barcode || "");
+const printLabel = (item) => {
+  const barcode = String(item.barcode);
 
-  return (
-    <div
-      style={{
-        width: "50mm",
-        height: "25mm",
-        padding: "2mm",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        fontFamily: "Arial",
-        overflow: "hidden",
-      }}
-    >
-      {/* NAME */}
-      <div
-        style={{
-          fontSize: "10px",
-          fontWeight: "bold",
-          textAlign: "center",
-          maxHeight: "8mm",
-          overflow: "hidden",
-        }}
-      >
-        {item.name}
-      </div>
+  const printWindow = window.open("", "_blank", "width=400,height=200");
 
-      {/* SVG BARCODE (RELIABLE IN PRINT) */}
-      <svg
-        width="100%"
-        height="14mm"
-        viewBox="0 0 200 50"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <text
-          x="50%"
-          y="30"
-          textAnchor="middle"
-          fontSize="14"
-          fontFamily="monospace"
-        >
-          {barcode}
-        </text>
-      </svg>
-    </div>
-  );
-}
+  printWindow.document.open();
+  printWindow.document.write(`
+    <html>
+      <head>
+        <title>Print Label</title>
+
+        <style>
+          @page {
+            size: 50mm 25mm;
+            margin: 0;
+          }
+
+          body {
+            margin: 0;
+            width: 50mm;
+            height: 25mm;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            font-family: Arial;
+          }
+
+          .name {
+            font-size: 10px;
+            font-weight: bold;
+            text-align: center;
+            max-height: 8mm;
+            overflow: hidden;
+          }
+
+          .barcode {
+            font-family: monospace;
+            font-size: 14px;
+            margin-top: 2mm;
+          }
+        </style>
+      </head>
+
+      <body>
+        <div class="name">${item.name || ""}</div>
+        <div class="barcode">${barcode}</div>
+      </body>
+    </html>
+  `);
+
+  printWindow.document.close();
+
+  printWindow.onload = () => {
+    printWindow.focus();
+    printWindow.print();
+    printWindow.close();
+  };
+};
